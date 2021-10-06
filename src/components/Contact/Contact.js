f94338873e25f09da5883711aeaf1fdd3a9a647a
Jonathan Kwee <jonkwee@gmail.com>
2021-10-05 20:48:42 -0500

    Changed some margins

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,8 +5,8 @@ function Contact() {
         <div className="contact" id="contact">
             <h1 className="contact_title">Contact</h1>
             <p className="contact_content">
-                Want to request for my resume or just want to say hi?
-                Send me an email at
+                {`Want to request for my resume or just want to say hi?
+                Send me an email at `}
                 <a href="mailto: jonkwee@gmail.com">jonkwee@gmail.com</a>.
             </p>
         </div>
